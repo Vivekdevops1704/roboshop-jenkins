93f5cc6a5189eb1066fd35ad75c6ffef77ee67b2
vivek <vivekemails17@gmail.com>
2023-11-20 17:44:23 +0530

    added groovy

--- shared-library/vars/demo.groovy
+++ shared-library/vars/demo.groovy
@@ -1,3 +1,38 @@
 def info(message) {
-    echo "INFO: ${message}""
+    echo "INFO: ${message}"
+}
+def call() {
+    pipeline {
+    agent any
+       stages {
+            stage("compile"){
+                steps{
+                    echo "this is compile phase"
+                    script {
+                      info 'starting'
+                    }
+                }
+            }
+            stage("build"){
+                steps{
+                    echo "this is build phase"
+                }
+            }
+            stage("test"){
+                steps{
+                    echo "this is test phase"
+                }
+            }
+            stage("sast check"){
+                steps{
+                    echo "this is sonarqibe scan phase"
+                }
+            }
+            stage("test check"){
+                steps{
+                    echo "this is testing phase"
+                }
+            }
+        }
+    }
 }
