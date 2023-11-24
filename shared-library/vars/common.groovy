8577b7abc934676b17e6496b2b748acd72b82aea
Vivekdevops1704 <150699648+Vivekdevops1704@users.noreply.github.com>
2023-11-25 02:29:22 +0530

    Update common.groovy

--- shared-library/vars/common.groovy
+++ shared-library/vars/common.groovy
@@ -1,6 +1,6 @@
 def compile() {
     if (env.codeType == "maven"){
-         sh '/opt/maven/bin/mvn --package'
+         sh '/opt/maven/bin/mvn package'
     }
     if (env.codeType == "nodejs"){
         print 'nodejs'
