efc3a567cb565cc7cfe1e122582114ce304d1835
Vivekdevops1704 <150699648+Vivekdevops1704@users.noreply.github.com>
2023-11-26 21:57:08 +0530

    Update common.groovy

--- shared-library/vars/common.groovy
+++ shared-library/vars/common.groovy
@@ -30,6 +30,10 @@ def test() {
 
 def codeQuality() {
   stage('Code Quality') {
-      sh 'sonar-scanner -Dsonar.host.url=http://54.90.236.64:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.projectKey=${component} -Dsonar.qualitygate.wait=true'
+   def  sonaruser = sh (script: 'aws ssm get-parameter --name "sonarqube_user" --with-decryption --query="Parameter.Value", returnStdout: true).trim()
+   def  sonarpass = sh (script: 'aws ssm get-parameter --name "sonarqube_pass" --with-decryption --query="Parameter.Value", returnStdout: true).trim()                    
+    //  sh 'sonar-scanner -Dsonar.host.url=http://54.90.236.64:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.projectKey=${component} -Dsonar.qualitygate.wait=true'
+   print $sonaruser
+   print $sonarpass                     
     }
     }
