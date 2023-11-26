e3a1d1e7918fa9999a19f65af59a2ce5740ca414
Vivekdevops1704 <150699648+Vivekdevops1704@users.noreply.github.com>
2023-11-27 01:58:43 +0530

    Update common.groovy

--- shared-library/vars/common.groovy
+++ shared-library/vars/common.groovy
@@ -33,7 +33,8 @@ def codeQuality() {
   stage('Code Quality') {
    env.sonaruser = sh (script: 'aws ssm get-parameter --name "sonarqube_user" --query="Parameter.Value" |xargs', returnStdout: true).trim()
    env.sonarpass = sh (script: 'aws ssm get-parameter --name "sonarqube_pass" --with-decryption --query="Parameter.Value" |xargs', returnStdout: true).trim()
-    wrap([$class: "MaskPasswordsBuildWrapper", varPasswordPairs: [[password: sonarpass]]])  
+    wrap([$class: "MaskPasswordsBuildWrapper", varPasswordPairs: [[password: sonarpass]]]) {
     sh 'sonar-scanner -X -Dsonar.host.url=http://3.89.200.49:9000 -Dsonar.login=${sonaruser} -Dsonar.password=${sonarpass} -Dsonar.projectKey=${component} -Dsonar.qualitygate.wait=true'
    }
+}
 }
