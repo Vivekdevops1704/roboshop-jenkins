67e1c65132acfd8ac7bfbee069518de8adfadff4
vivek <vivekemails17@gmail.com>
2023-11-27 21:49:37 +0530

    adding ssm params

--- shared-library/vars/common.groovy
+++ shared-library/vars/common.groovy
@@ -36,10 +36,12 @@ def codeQuality() {
     wrap([$class: "MaskPasswordsBuildWrapper", varPasswordPairs: [[password: sonarpass]]]) {
     if(env.codeType == "maven") {
         //sh 'sonar-scanner -Dsonar.host.url=http://172.31.89.117:9000 -Dsonar.login=${sonaruser} -Dsonar.password=${sonarpass} -Dsonar.projectKey=${component} -Dsonar.qualitygate.wait=true -Dsonar.java.binaries=./target'
-    sh 'sonar-scanner -X -Dsonar.host.url=http://3.89.200.49:9000 -Dsonar.login=${sonaruser} -Dsonar.password=${sonarpass} -Dsonar.projectKey=${component} -Dsonar.qualitygate.wait=true -Dsonar.java.binaries=./target'
+    //sh 'sonar-scanner -X -Dsonar.host.url=http://3.89.200.49:9000 -Dsonar.login=${sonaruser} -Dsonar.password=${sonarpass} -Dsonar.projectKey=${component} -Dsonar.qualitygate.wait=true -Dsonar.java.binaries=./target'
+   print 'OK'
    }
         else {
-             sh 'sonar-scanner -X -Dsonar.host.url=http://3.89.200.49:9000 -Dsonar.login=${sonaruser} -Dsonar.password=${sonarpass} -Dsonar.projectKey=${component} -Dsonar.qualitygate.wait=true'
+           //  sh 'sonar-scanner -X -Dsonar.host.url=http://3.89.200.49:9000 -Dsonar.login=${sonaruser} -Dsonar.password=${sonarpass} -Dsonar.projectKey=${component} -Dsonar.qualitygate.wait=true'
+         print 'Ok'
          }
     }
 }
