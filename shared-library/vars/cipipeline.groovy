fc5fb9a41a78dffd161bc3ee630cae2f96e495be
Vivekdevops1704 <150699648+Vivekdevops1704@users.noreply.github.com>
2023-11-27 22:41:55 +0530

    Update cipipeline.groovy

--- shared-library/vars/cipipeline.groovy
+++ shared-library/vars/cipipeline.groovy
@@ -24,7 +24,7 @@ node {
                     echo "this is security phase"               
             }
             stage("release"){
-                    echo "Hi Release"
+                    common.codeRelease()
                 }
             }
         }
